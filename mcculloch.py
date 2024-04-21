4433328eeec1110f383fa5168e808ff1babec628
Rawan Ahmed Abdelghfar <rewanahmedali16@gmail.com>
2024-04-21 14:45:52 +0200

    mcculloch

--- mcculloch.py
+++ mcculloch.py
@@ -12,6 +12,31 @@ class McCullochPittsNeuron:
         activations = np.where(weighted_sum >= self.threshold, 1, 0)  # Apply threshold
         return activations
 
+
+data = pd.read_csv('heart.csv')
+
+# Extract features and target
+X = data[['age', 'trestbps']].values
+y = data['target'].values
+
+weights = [0.5, 0.5]  
+threshold = 0  
+
+
+neuron = McCullochPittsNeuron(weights, threshold)
+
+predictions = neuron.activate(X)
+accuracy = np.mean(predictions == y)
+print("Accuracy: ",accuracy)
+
+
+new_data_point = np.array([[60, 130]])  
+prediction = neuron.activate(new_data_point)
+if prediction[0] == 1:
+    print("The person is predicted to be positive for heart disease.")
+else:
+    print("The person is predicted not to have heart disease.")
+
 def plot_decision_boundary(neuron, X, y):
     x_min, x_max = X[:, 0].min() - 0.1, X[:, 0].max() + 0.1
     y_min, y_max = X[:, 1].min() - 0.1, X[:, 1].max() + 0.1
@@ -28,32 +53,4 @@ def plot_decision_boundary(neuron, X, y):
     plt.scatter(X[:, 0], X[:, 1], c=y, cmap=plt.cm.Spectral, edgecolors='k')
     plt.show()
 
-# Read the dataset
-data = pd.read_csv('heart.csv')
-
-# Extract features and target
-X = data[['age', 'trestbps']].values
-y = data['target'].values
-
-# Define weights and threshold for the neuron
-weights = [0.5, 0.5]  # Adjust these weights as needed
-threshold = 1  # Adjust threshold as needed
-
-# Create a McCulloch-Pitts neuron
-neuron = McCullochPittsNeuron(weights, threshold)
-
-# Calculate accuracy
-predictions = neuron.activate(X)
-accuracy = np.mean(predictions == y) * 100
-print("Accuracy: {:.2f}%".format(accuracy))
-
-# Make a prediction for a new data point
-new_data_point = np.array([[60, 130]])  # Adjust as needed
-prediction = neuron.activate(new_data_point)
-if prediction[0] == 1:
-    print("The person is predicted to have heart disease.")
-else:
-    print("The person is predicted not to have heart disease.")
-
-# Plot the decision boundary
 plot_decision_boundary(neuron, X, y)
