45b458f9ee1b3ef410774b8c1cd06f82ba431c64
Rawan Ahmed Abdelghfar <rewanahmedali16@gmail.com>
2024-04-21 19:50:59 +0200

    MLP using backpropagation technology

--- backpropagation.py
+++ backpropagation.py
@@ -45,18 +45,61 @@ Y_pred = model.predict(X)
 y_pred = np.round(Y_pred).flatten()
 
 # Calculate classification report
-from sklearn.metrics import classification_report
-print("Classification Report:")
-print(classification_report(Y, y_pred))
-
-def confusion_matrix_custom(y_true, y_pred):
+def calculate_classification_report(y_true, y_pred):
     TP = np.sum(np.logical_and(y_true == 1, y_pred == 1))
     TN = np.sum(np.logical_and(y_true == 0, y_pred == 0))
     FP = np.sum(np.logical_and(y_true == 0, y_pred == 1))
     FN = np.sum(np.logical_and(y_true == 1, y_pred == 0))
     
-    return np.array([[TN, FP], [FN, TP]])
+    precision_0 = TN / (TN + FP)
+    recall_0 = TN / (TN + FN)
+    f1_score_0 = 2 * precision_0 * recall_0 / (precision_0 + recall_0)
+    
+    precision_1 = TP / (TP + FP)
+    recall_1 = TP / (TP + FN)
+    f1_score_1 = 2 * precision_1 * recall_1 / (precision_1 + recall_1)
 
+    accuracy = (TP + TN) / (TP + TN + FP + FN)
+
+    return {
+        '0.0': {'precision': precision_0, 'recall': recall_0, 'f1-score': f1_score_0, 'support': len(y_true) - np.sum(y_true)},
+        '1.0': {'precision': precision_1, 'recall': recall_1, 'f1-score': f1_score_1, 'support': np.sum(y_true)},
+        'accuracy': accuracy,
+        'macro avg': {'precision': (precision_0 + precision_1) / 2, 'recall': (recall_0 + recall_1) / 2, 'f1-score': (f1_score_0 + f1_score_1) / 2, 'support': len(y_true)},
+        'weighted avg': {'precision': (precision_0 * (len(y_true) - np.sum(y_true)) + precision_1 * np.sum(y_true)) / len(y_true), 
+                         'recall': (recall_0 * (len(y_true) - np.sum(y_true)) + recall_1 * np.sum(y_true)) / len(y_true), 
+                         'f1-score': (f1_score_0 * (len(y_true) - np.sum(y_true)) + f1_score_1 * np.sum(y_true)) / len(y_true), 
+                         'support': len(y_true)}
+    }
+
+classification_result = calculate_classification_report(Y, y_pred)
+print("Classification Report:")
+print("{:<45} {:<12} {:<12} {:<12} {:<12}".format("", "precision", "recall", "f1-score", "support"))
+print("{:<45} {:<12} {:<12} {:<12} {:<12}".format("0.0", f"{classification_result['0.0']['precision']:.2f}", 
+                                                  f"{classification_result['0.0']['recall']:.2f}", 
+                                                  f"{classification_result['0.0']['f1-score']:.2f}", 
+                                                  classification_result['0.0']['support']))
+print("{:<45} {:<12} {:<12} {:<12} {:<12}".format("1.0", f"{classification_result['1.0']['precision']:.2f}", 
+                                                  f"{classification_result['1.0']['recall']:.2f}", 
+                                                  f"{classification_result['1.0']['f1-score']:.2f}", 
+                                                  classification_result['1.0']['support']))
+print("{:<45} {:<12} {:<12} {:<12} {:<12}".format("accuracy", "", "", f"{classification_result['accuracy']:.2f}", ""))
+print("{:<45} {:<12} {:<12} {:<12} {:<12}".format("macro avg", f"{classification_result['macro avg']['precision']:.2f}", 
+                                                  f"{classification_result['macro avg']['recall']:.2f}", 
+                                                  f"{classification_result['macro avg']['f1-score']:.2f}", 
+                                                  classification_result['macro avg']['support']))
+print("{:<45} {:<12} {:<12} {:<12} {:<12}".format("weighted avg", f"{classification_result['weighted avg']['precision']:.2f}", 
+                                                  f"{classification_result['weighted avg']['recall']:.2f}", 
+                                                  f"{classification_result['weighted avg']['f1-score']:.2f}", 
+                                                  classification_result['weighted avg']['support']))
+
+
+def confusion_matrix_custom(y_true, y_pred):
+    TP = np.sum(np.logical_and(y_true == 1, y_pred == 1))
+    TN = np.sum(np.logical_and(y_true == 0, y_pred == 0))
+    FP = np.sum(np.logical_and(y_true == 0, y_pred == 1))
+    FN = np.sum(np.logical_and(y_true == 1, y_pred == 0))   
+    return np.array([[TN, FP], [FN, TP]])
 print("Confusion Matrix:")
 print(confusion_matrix_custom(Y, y_pred))
 
