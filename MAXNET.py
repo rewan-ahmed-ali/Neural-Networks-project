e8863f2f8468a9940c2158fede7554168aace47a
Rawan Ahmed Abdelghfar <rewanahmedali16@gmail.com>
2024-04-27 07:23:36 +0300

    MAXNET

--- MAXNET.py
+++ MAXNET.py
@@ -9,14 +9,23 @@ data = pd.read_csv("heart.csv")
 X = data.drop(columns='target').values
 y = data['target'].values
 
-
 # تحويل التصنيفات إلى تنسيق فئوي واحد
 num_classes = len(np.unique(y))
 y = tf.keras.utils.to_categorical(y, num_classes)
 
 # تقسيم البيانات إلى مجموعة التدريب ومجموعة الاختبار
-from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+# نسبة الاختبار: 20%
+test_size = 0.2
+num_samples = len(X)
+num_test_samples = int(test_size * num_samples)
+
+# اختيار عشوائي لمؤشرات البيانات لمجموعة الاختبار
+test_indices = np.random.choice(num_samples, num_test_samples, replace=False)
+train_indices = np.delete(np.arange(num_samples), test_indices)
+
+# تقسيم البيانات إلى مجموعة التدريب ومجموعة الاختبار
+X_train, X_test = X[train_indices], X[test_indices]
+y_train, y_test = y[train_indices], y[test_indices]
 
 # تحديد النموذج
 model = tf.keras.Sequential([
@@ -24,7 +33,7 @@ model = tf.keras.Sequential([
     tf.keras.layers.Dense(32, activation='relu'),
     tf.keras.layers.Dense(num_classes, activation='softmax')
 ])
-
+model.summary()
 # تحديد المعيار ودالة الخسارة
 model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
 
