d3bcdc0046622a0f626f194904a0de0a48d334cc
Rawan Ahmed Abdelghfar <rewanahmedali16@gmail.com>
2024-04-27 08:51:03 +0300

    MEXICAN HAT

--- MEXICAN HAT.py
+++ MEXICAN HAT.py
@@ -23,32 +23,29 @@ w2 = np.array([C1] * (2 * R1 + 1))
 w3 = np.array([C2] * (R2 - R1))
 w = np.concatenate((w1, w2, w3))
 
-# Array Status as matrix
-print("Array Status")
-for i in range(len(X)):
-    sum = 0
-    k = -R1
-    array_status = []
-    while k <= R1:
-        sum = sum + C1 * X_new[i + k]
-        array_status.append(C1 * X_new[i + k])
-        k = k + 1
-    k = -R2
-    while k < -R1:
-        sum = sum + C2 * X_new[i + k]
-        array_status.insert(0, C2 * X_new[i + k])
-        k = k + 1
-    k = R1 + 1
-    while k <= R2:
-        sum = sum + C2 * X_new[i + k]
-        array_status.append(C2 * X_new[i + k])
-        k = k + 1
-    print(np.array(array_status))
-
 # Applying Mexican Hat Net
 output = np.zeros(len(X))
 
 for i in range(0, len(X)):
     output[i] = np.dot(w, X_new[i:i + len(w)])
-print('')
+
 print("Output:", output)
+
+# Add Array Status
+print('')
+print("Array Status")
+for i in range(len(X)):
+    sum = 0
+    k = -R1
+    while k <= R1:
+        sum = sum + C1 * X_new[i + k]
+        k = k + 1
+    k = -R2
+    while k < -R1:
+        sum = sum + C2 * X_new[i + k]
+        k = k + 1
+    k = R1 + 1
+    while k <= R2:
+        sum = sum + C2 * X_new[i + k]
+        k = k + 1
+    print(sum)
