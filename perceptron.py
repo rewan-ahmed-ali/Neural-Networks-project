d316228f04f68f0e51b9eb80be8367db1e893a89
Rawan Ahmed Abdelghfar <rewanahmedali16@gmail.com>
2024-05-03 23:11:36 +0300

    perceptron

--- perceptron.py
+++ perceptron.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 
 class Perceptron:
-    def __init__(self, learning_rate=0.01, epochs=100):
+    def __init__(self, learning_rate=0.1, epochs=100):
         self.learning_rate = learning_rate
         self.epochs = epochs
         self.weights = None
@@ -16,27 +16,29 @@ class Perceptron:
         for _ in range(self.epochs):
             for i in range(n_samples):
                 y_predicted = np.dot(X[i], self.weights) + self.bias
-                if y_predicted >= 0:
-                    y_predicted = 1
-                else:
-                    y_predicted = 0
 
-                update = self.learning_rate * (y[i] - y_predicted)
-                self.weights += update * X[i]
-                self.bias += update
+                if y_predicted != y[i]:
+                    self.weights = self.weights + self.learning_rate * (y[i] * X[i])
+                    self.bias = self.bias + self.learning_rate * (y[i])
+
+    def activation_function(self, y_predicted):
+        if y_predicted > 0:
+            return 1
+        elif y_predicted == 0:
+            return 0
+        else:
+            return -1
 
     def predict(self, X):
         y_predicted = np.dot(X, self.weights) + self.bias
-        return np.where(y_predicted >= 0, 1, 0)
+        return np.array([self.activation_function(y) for y in y_predicted])
 
-# Load the heart diseases dataset
+
 heart_data = pd.read_csv("heart.csv")
 
-# Splitting the features and target
 X = heart_data.drop(columns='target').values
 y = heart_data['target'].values
 
-# Splitting the data into training and testing sets
 def split_data(X, y, test_size=0.2):
     n_samples = X.shape[0]
     n_test = int(n_samples * test_size)
@@ -46,42 +48,30 @@ def split_data(X, y, test_size=0.2):
 
 X_train, X_test, y_train, y_test = split_data(X, y)
 
-# Create and train the Perceptron
 perceptron = Perceptron()
 perceptron.train(X_train, y_train)
 
-# Accuracy on training data
 y_train_pred = perceptron.predict(X_train)
 train_accuracy = np.mean(y_train_pred == y_train)
 print("Accuracy on training data:", train_accuracy)
 
-# Accuracy on test data
 y_test_pred = perceptron.predict(X_test)
 test_accuracy = np.mean(y_test_pred == y_test)
 print("Custom Perceptron Accuracy:", test_accuracy)
 
-def calculate_classification_report(y_true, y_pred):
-    TP = np.sum(np.logical_and(y_true == 1, y_pred == 1))
-    TN = np.sum(np.logical_and(y_true == 0, y_pred == 0))
-    FP = np.sum(np.logical_and(y_true == 0, y_pred == 1))
-    FN = np.sum(np.logical_and(y_true == 1, y_pred == 0))
-    precision_0 = TN / (TN + FP)
-    recall_0 = TN / (TN + FN)
-    f1_score_0 = 2 * precision_0 * recall_0 / (precision_0 + recall_0)
-    precision_1 = TP / (TP + FP)
-    recall_1 = TP / (TP + FN)
-    f1_score_1 = 2 * precision_1 * recall_1 / (precision_1 + recall_1)
-    accuracy = (TP + TN) / (TP + TN + FP + FN)
-    return {
-        '0.0': {'precision': precision_0, 'recall': recall_0, 'f1-score': f1_score_0, 'support': len(y_true) - np.sum(y_true)},
-        '1.0': {'precision': precision_1, 'recall': recall_1, 'f1-score': f1_score_1, 'support': np.sum(y_true)},
-        'accuracy': accuracy,
-        'macro avg': {'precision': (precision_0 + precision_1) / 2, 'recall': (recall_0 + recall_1) / 2, 'f1-score': (f1_score_0 + f1_score_1) / 2, 'support': len(y_true)},
-        'weighted avg': {'precision': (precision_0 * (len(y_true) - np.sum(y_true)) + precision_1 * np.sum(y_true)) / len(y_true), 
-                         'recall': (recall_0 * (len(y_true) - np.sum(y_true)) + recall_1 * np.sum(y_true)) / len(y_true), 
-                         'f1-score': (f1_score_0 * (len(y_true) - np.sum(y_true)) + f1_score_1 * np.sum(y_true)) / len(y_true), 
-                         'support': len(y_true)}
-    }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 def calculate_classification_report(y_true, y_pred):
     TP = np.sum(np.logical_and(y_true == 1, y_pred == 1))
