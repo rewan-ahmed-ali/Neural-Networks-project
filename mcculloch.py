61adbc0320c020a63339df9be4f07e7793a82557
Rawan Ahmed Abdelghfar <rewanahmedali16@gmail.com>
2024-05-03 17:32:22 +0300

    mcculloch

--- mcculloch.py
+++ mcculloch.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 
 class McCullochPittsNeuron:
     def __init__(self, weights, threshold):
@@ -16,41 +15,28 @@ class McCullochPittsNeuron:
 data = pd.read_csv('heart.csv')
 
 # Extract features and target
-X = data[['age', 'trestbps']].values
+X = data.drop('target', axis=1).values
 y = data['target'].values
 
-weights = [0.5, 0.5]  
-threshold = 0  
+num_features = X.shape[1]
+weights = np.array([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]) 
 
+# تحديد حاجز العتبة بناءً على عدد المدخلات والأوزان الموجبة والسالبة
+threshold = num_features * np.sum(weights > 0) - np.sum(weights)
 
 neuron = McCullochPittsNeuron(weights, threshold)
 
 predictions = neuron.activate(X)
 accuracy = np.mean(predictions == y)
-print("Accuracy: ",accuracy)
 
-
-new_data_point = np.array([[60, 130]])  
+new_data_point = np.array([[71,0,0,112,149,0,1,125,0,1.6,1,0,2]])  
 prediction = neuron.activate(new_data_point)
+
+# print("New data point:", new_data_point)
+print("Prediction |", prediction)
+print("Accuracy |", accuracy)
+print("threshold |",threshold)
 if prediction[0] == 1:
     print("The person is predicted to be positive for heart disease.")
 else:
     print("The person is predicted not to have heart disease.")
-
-def plot_decision_boundary(neuron, X, y):
-    x_min, x_max = X[:, 0].min() - 0.1, X[:, 0].max() + 0.1
-    y_min, y_max = X[:, 1].min() - 0.1, X[:, 1].max() + 0.1
-    xx, yy = np.meshgrid(np.arange(x_min, x_max, 0.01),
-                         np.arange(y_min, y_max, 0.01))
-    Z = neuron.activate(np.c_[xx.ravel(), yy.ravel()])
-    Z = Z.reshape(xx.shape)
-
-    plt.contourf(xx, yy, Z, alpha=0.3)
-    plt.xlabel('Age')
-    plt.ylabel('Resting Blood Pressure')
-    plt.title('Decision Boundary')
-    plt.grid(True)
-    plt.scatter(X[:, 0], X[:, 1], c=y, cmap=plt.cm.Spectral, edgecolors='k')
-    plt.show()
-
-plot_decision_boundary(neuron, X, y)
