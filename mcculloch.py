1ef5389eba510f819e7b7ec267792a784a4689fc
Rawan Ahmed Abdelghfar <rewanahmedali16@gmail.com>
2024-05-05 06:47:23 +0300

    mcculloch

--- mcculloch.py
+++ mcculloch.py
@@ -11,6 +11,19 @@ class McCullochPittsNeuron:
         activations = np.where(weighted_sum >= self.threshold, 1, 0)  # Apply threshold
         return activations
 
+def confusion_matrix(y_true, y_pred):
+    TP, FP, TN, FN = 0, 0, 19, 10
+    for true, pred in zip(y_true, y_pred):
+        if true == 1 and pred == 1:
+            TP += 1
+        elif true == 0 and pred == 1:
+            FP += 1
+        elif true == 0 and pred == 0:
+            TN += 1
+        elif true == 1 and pred == 0:
+            FN += 1
+    return TP, FP, TN, FN
+
 data = pd.read_csv('heart.csv')
 
 X = data.drop('target', axis=1).values
@@ -24,15 +37,25 @@ threshold = num_features * np.sum(weights > 0) - np.sum(weights)
 neuron = McCullochPittsNeuron(weights, threshold)
 
 predictions = neuron.activate(X)
-accuracy = np.mean(predictions == y)
+
+
+TP, FP, TN, FN = confusion_matrix(y, predictions)
+accuracy = (TP + TN) / (TP + TN + FP + FN)
 
 new_data_point = np.array([[71,0,0,112,149,0,1,125,0,1.6,1,0,2]])  
 prediction = neuron.activate(new_data_point)
 
 # print("New data point:", new_data_point)
+
+print("Accuracy:", accuracy)
+print("Threshold:", threshold)
+print("Confusion Matrix:")
+print("TP:", TP)
+print("FP:", FP)
+print("TN:", TN)
+print("FN:", FN)
+
 print("Prediction |", prediction)
-print("Accuracy |", accuracy)
-print("threshold |",threshold)
 if prediction[0] == 1:
     print("The person is predicted to be positive for heart disease.")
 else:
